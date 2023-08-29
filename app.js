2d69e9253f3ede21d4e9e799af3b8d6c47dde452
Anhkhoa-dev <81515197+Anhkhoa-dev@users.noreply.github.com>
2023-08-29 21:55:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 app.use('/book', bookRouter);
 
 // catch 404 and forward to error handler
